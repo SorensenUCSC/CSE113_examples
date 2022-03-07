a0a607b0571dfc841b52b34f2960943c74beae23
Tyler <tyler.sorensen@outlook.com>
2022-03-07 14:36:58 -0500

    added first GPU codes

---
+++ CPU_vs_GPU/GPUSingle/main.cu
@@ -0,0 +1,60 @@
+#include <thread>
+#include <assert.h>
+#include <chrono>
+#include <iostream>
+using namespace std::chrono;
+using namespace std;
+
+#define SIZE (1024*1024)
+#define OUTER (16)
+
+__global__ void vector_add(double * d_a, double * d_b, double * d_c, int size) {
+  for (int j = 0; j < OUTER; j++) {
+    for (int i = 0; i < size; i++) {
+      d_a[i] = d_b[i] + d_c[i];
+    }
+  }
+}
+
+
+int main() {
+
+  double *a = new double[SIZE];
+  double *b = new double[SIZE];
+  double *c = new double[SIZE];
+
+  for(int i = 0; i < SIZE; i++) {
+    a[i] = 0;
+    b[i] = i;
+    c[i] = 1;
+  }
+
+  double *d_a, *d_b, *d_c;
+  int e = 0;
+  e = cudaMalloc(&d_a, SIZE*sizeof(double));
+  e |= cudaMalloc(&d_b, SIZE*sizeof(double));
+  e |= cudaMalloc(&d_c, SIZE*sizeof(double));
+
+  e |= cudaMemcpy(d_a, a, SIZE*sizeof(double), cudaMemcpyHostToDevice);
+  e |= cudaMemcpy(d_b, b, SIZE*sizeof(double), cudaMemcpyHostToDevice);
+  e |= cudaMemcpy(d_c, c, SIZE*sizeof(double), cudaMemcpyHostToDevice);
+
+  auto time_start = high_resolution_clock::now();
+  vector_add<<<1,1>>>(d_a, d_b, d_c, SIZE);
+  e |= cudaDeviceSynchronize();
+
+  auto time_end = high_resolution_clock::now();
+  auto time_duration = duration_cast<nanoseconds>(time_end - time_start);
+  double time_seconds = time_duration.count()/1000000000.0;
+  
+  cout << "timings: " << time_seconds << endl;
+      
+  for (int i = 0; i < SIZE; i++) {
+    assert(a[i] == (i + 1));    
+  }  
+
+  return 0;
+}
+
+
+  
