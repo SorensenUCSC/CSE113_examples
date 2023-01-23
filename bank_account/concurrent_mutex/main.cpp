377aacf5bb98ac7e2a3d7f04e1063826ac568c63
Tyler Sorensen <tyler.sorensen@outlook.com>
2023-01-22 21:39:43 -0800

    added two threads

--- bank_account/concurrent_mutex/main.cpp
+++ bank_account/concurrent_mutex/main.cpp
@@ -11,9 +11,11 @@ int tylers_account = 0;
 mutex tylers_account_mutex;
 
 void ucsc_pays_tyler() {
-  tylers_account_mutex.lock();
-  tylers_account += 1;
-  tylers_account_mutex.unlock();
+  for (int i = 0; i < HOURS; i++) {
+    tylers_account_mutex.lock();
+    tylers_account += 1;
+    tylers_account_mutex.unlock();
+  }
 }
 
 // What happens if we move the mutex outside the 'for' loop?
@@ -27,12 +29,11 @@ void tyler_buys_coffee() {
 
 int main() {
 
-  thread t = thread(tyler_buys_coffee);
+  thread t0 = thread(tyler_buys_coffee);
+  thread t1 = thread(ucsc_pays_tyler);
 
-  for (int i = 0; i < HOURS; i++) {
-    ucsc_pays_tyler();
-  }
-  t.join();
+  t0.join();
+  t1.join();
 
   cout << "tyler's account balance: " << tylers_account << endl;
 
