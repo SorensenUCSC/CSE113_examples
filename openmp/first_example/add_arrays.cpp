8a6acd268adc23108a0976d775e47898daca6158
Tyler <tyler.sorensen@outlook.com>
2023-02-26 18:14:13 -0800

    added command line argument

--- openmp/first_example/add_arrays.cpp
+++ openmp/first_example/add_arrays.cpp
@@ -1,10 +1,11 @@
 #include <chrono>
 #include <stdlib.h>
 #include <iostream>
+#include <omp.h>
 #define SIZE (1024*1024*128)
 #define ITERS (16)
 
-int main() {
+int main(int argc, char ** argv) {
   int *a, *b, *c;
   a = (int *) malloc(sizeof(int)*SIZE);
   b = (int *) malloc(sizeof(int)*SIZE);
@@ -13,6 +14,14 @@ int main() {
   for (int i = 0; i < SIZE; i++) {
     c[i] = b[i] = a[i] = 0;
   }
+
+  int num_threads = 8;
+  if (argc > 1) {
+    num_threads = atoi(argv[1]);
+  }
+  std::cout << "num threads " << num_threads << std::endl;
+
+  omp_set_num_threads(num_threads);
 
 
   auto start = std::chrono::high_resolution_clock::now();
