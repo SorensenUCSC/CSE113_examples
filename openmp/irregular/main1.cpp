2e8d52626cdca91d97b92ab4080b6df100b4cdd4
Tyler <tyler.sorensen@outlook.com>
2023-02-26 18:17:26 -0800

    added command line arg

--- openmp/irregular/main1.cpp
+++ openmp/irregular/main1.cpp
@@ -2,12 +2,13 @@
 #include "util.h"
 #include <chrono>
 #include <iostream>
+#include <omp.h>
 #include "checker.h"
 
 ScheduleType t = static_t;
 
 
-int main() {
+int main(int argc, char **argv) {
   float* result_parallel = new float[SIZE];
   int* mult = new int[SIZE];
   for (int i = 0; i < SIZE; i++) {
@@ -15,9 +16,20 @@ int main() {
   }
   linear_work(mult, SIZE);
 
+  int num_threads = 4;
+  if (argc > 1) {
+    num_threads = atoi(argv[1]);
+  }
+  std::cout << "num threads " << num_threads << std::endl;
+
+  omp_set_num_threads(num_threads);
+
+
+  // Launch NUM_THREADS threads and then join them
+
   auto start = std::chrono::high_resolution_clock::now();
 
-#pragma omp parallel for num_threads(2) schedule(dynamic)
+#pragma omp parallel for schedule(static)
   for (int i = 0; i < SIZE; i++) {
     float base = result_parallel[i];
     for (int w = 0; w < mult[i]-1; w++) {
@@ -25,8 +37,6 @@ int main() {
     }
   }
 
-
-  // Launch NUM_THREADS threads and then join them
 
   auto stop = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
